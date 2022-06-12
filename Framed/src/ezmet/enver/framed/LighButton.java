ee5472665b8c81a7cd0ff47e4cbb3ade08fb6558
EnverJ <Enlem1987@gmail.com>
2022-06-11 22:18:57 -0600

    LighButton

--- Framed/src/ezmet/enver/framed/LighButton.java
+++ Framed/src/ezmet/enver/framed/LighButton.java
@@ -7,43 +7,48 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 public class LighButton extends JButton {
-private static final long serialVersionUID=1L;
-private final int MAXSIZE=50;
-private int row=0;
-private int col=0;
-public LighButton(int row, int col){
-	this.row=row;
-	this.col=col;
-	setBackground(Color.black);
-	Dimension size=new Dimension(MAXSIZE,MAXSIZE);
-	setPreferredSize(size);
-	
-}
-public int getRow(){
-	return row;
-}
-public int getCol(){
-	return col;
-}
+	private static final long serialVersionUID = 1L;
+	private final int MAXSIZE = 50;
+	private int row = 0;
+	private int col = 0;
 
-public void turnOn(){
-	setBackground(Color.red);
-}
-public void turnOff(){
-	setBackground(Color.black);
-}
-public boolean isLit(){
-	Color color=getBackground();
-	boolean isLit=color.equals(Color.red);
-	return isLit;
-}
-public void toggle(){
-	if(isLit()){
-		turnOff();
-		
+	public LighButton(int row, int col) {
+		this.row = row;
+		this.col = col;
+		setBackground(Color.black);
+		Dimension size = new Dimension(MAXSIZE, MAXSIZE);
+		setPreferredSize(size);
+
 	}
-	else{
-		turnOn();
+
+	public int getRow() {
+		return row;
 	}
-}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void turnOn() {
+		setBackground(Color.red);
+	}
+
+	public void turnOff() {
+		setBackground(Color.black);
+	}
+
+	public boolean isLit() {
+		Color color = getBackground();
+		boolean isLit = color.equals(Color.red);
+		return isLit;
+	}
+
+	public void toggle() {
+		if (isLit()) {
+			turnOff();
+
+		} else {
+			turnOn();
+		}
+	}
 }
