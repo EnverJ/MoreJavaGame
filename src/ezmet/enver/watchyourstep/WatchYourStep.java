527a0659c7d35693fefa88736b8fc68031fddf83
EnverJ <enlem1987@gmail.com>
2022-06-28 23:32:51 -0600

    aa

--- src/ezmet/enver/watchyourstep/WatchYourStep.java
+++ src/ezmet/enver/watchyourstep/WatchYourStep.java
@@ -1,16 +1,45 @@
 package ezmet.enver.watchyourstep;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import ezmet.enver.mycomponents.TitleLabel;
 
 public class WatchYourStep extends JFrame {
-	private static final long serialVersionUID=1L;
-	public WatchYourStep(){
-		
+	private static final long serialVersionUID = 1L;
+
+	public WatchYourStep() {
+		initGUI();
+		setTitle("Watch your Step");
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 	}
 
+	private void initGUI() {
+		TitleLabel titlelabel = new TitleLabel("Watch Your Step");
+		add(titlelabel, BorderLayout.PAGE_START);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// add a look and feel
+
+		try {
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+		} catch (Exception e) {
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new WatchYourStep();
+			}
+		});
 
 	}
 
